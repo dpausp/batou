f04894c9cbde47db8ea8a6a84404e5fb8a9df726
s-dev <s-dev@ts01.fcio.net>
2026-03-16 11:49:44 +0100

    Update GitHub Actions to use uv
    - Update actions to v4/v5 (checkout, setup-python)
    - Add astral-sh/setup-uv for uv integration
    - Use uvx with tox-uv-bare in main.yml
    - Update develop.sh to use uv sync
    - Delete redundant lint.yml (pre-commit.yaml covers it)
    - Update nix.yml to cachix/install-nix-action@v30

--- develop.sh
+++ develop.sh
@@ -2,7 +2,5 @@
 
 set -ex
 
-rm -rf .Python bin lib include .tox examples/*/.appenv
-
-python${1:-3} -m venv .
-bin/pip install --upgrade -r requirements-dev.txt
+# Sync all dependencies with uv (reads pyproject.toml + uv.lock)
+uv sync --all-groups
